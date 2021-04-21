5e582b6e443fa1d8d875ae7c9e816a8ad3254193
hazcoper <zemariamateus@gmai.com>
2021-04-21 11:24:02 +0100

    changed to our database

---
+++ v5 - all but sound/readme.txt
@@ -0,0 +1,1 @@
+this contains a version with everything but the sound
--- v5 - all but sound/sketchSound.js
+++ v5 - all but sound/sketchSound.js
@@ -7,7 +7,7 @@
 
 // Database (CHANGE THESE!)
 const GROUP_NUMBER   = 15;      // Add your group number here as an integer (e.g., 2, 3)
-const BAKE_OFF_DAY   = true;  // Set to 'true' before sharing during the simulation and bake-off days
+const BAKE_OFF_DAY   = false;  // Set to 'true' before sharing during the simulation and bake-off days
 const NUMBER_TARGETS = 16;
 const NUMBER_ATTEMPTS = 48;
 
